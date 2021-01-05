1e2afda172f260d5118d0cea0a86803447b4ad27
DiSon93 <sonnguyendi93@gmail.com>
2021-01-05 22:43:30 +0700

    first commit

---
+++ js/backToTop.js
@@ -0,0 +1,25 @@
+
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("button").style.right = "0";
+  } else {
+    document.getElementById("button").style.right = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
